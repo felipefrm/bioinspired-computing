5cd622089d3beec480d1de93bd006cab69441c76
Felipe <narulee@hotmail.com>
2020-03-11 11:55:49 -0300

    working

--- Algoritmo Genetico/ag.py
+++ Algoritmo Genetico/ag.py
@@ -8,11 +8,12 @@ class Individuo():
         self.avaliacao = None
 
 class AG():
-    def __init__(self, tam_populacao, num_geracoes, nbits):
+    def __init__(self, tam_populacao, num_geracoes, taxa_mutacao, nbits):
         self.tam_populacao = tam_populacao
         self.nbits = nbits
         self.populacao = None
         self.num_geracoes = num_geracoes
+        self.taxa_mutacao = taxa_mutacao
 
     def geraPopulacao(self):
         # gera a populacao de individuos em binarios de 12 bits
@@ -21,65 +22,60 @@ class AG():
 
     def defineIndividuos(self):
 
-        parametro2 = []
-        parametro1 = []
-
         # divide o binario de cada individuo em 2 genes
-        genes_individuos = []
         individuo = []
-        # print(ag.populacao)
         for ind in ag.populacao:
             individuo.append(Individuo(ind[:6], ind[6:]))
 
         return individuo
 
 
-    def avaliaPopulacao(self):
-        # aplica a função objetivo para cada individuo da população
-        print(individuo[0].x1 + individuo[0].x2)
-        for ind in individuo:
+    def avaliaPopulacao(self, populacao):
+        # aplica a função objetivo para cada individuo da população convertendo o binario em float
+        for ind in populacao:
             x1_float = float(int(''.join(str(e) for e in ind.x1), 2))
             x2_float = float(int(''.join(str(e) for e in ind.x2), 2))
             ind.avaliacao = func_obj([x1_float, x2_float])
 
-    def torneio(self):
+    def torneio(self, populacao):
 
         # o ultimo individuo não terá adversário, logo é automaticamente escolhido
-        if len(individuo) == 1:
-            vencedor = individuo[0]
-            individuo.remove(individuo[0])
+        if len(populacao) == 1:
+            vencedor = populacao[0]
+            populacao.remove(populacao[0])
+            # print("sou o ultimo")
             return vencedor
 
         # 2 individuos duelam, o que possui a maior avaliação na Fo "permanece"
-        # print(f"sample: {range(len(individuo))}")
-        sorteio = sample(range(len(individuo)), 2)
-        individuo1 = individuo[sorteio[0]]
+        # print(f"sample: {range(len(populacao))}")
+        sorteio = sample(range(len(populacao)), 2)
+        individuo1 = populacao[sorteio[0]]
         # print("peguei o individuo ", sorteio[0])
-        individuo2 = individuo[sorteio[1]]
+        individuo2 = populacao[sorteio[1]]
         # print("peguei o individuo ", sorteio[1])
         if individuo1.avaliacao >= individuo2.avaliacao:
             vencedor = individuo1
-            individuo.remove(individuo1)
+            populacao.remove(individuo1)
         else:
             vencedor = individuo2
-            individuo.remove(individuo2)
+            populacao.remove(individuo2)
         return vencedor
 
-    def elitismo(self):
+    def elitismo(self, populacao):
         avaliacoes = []
-        for ind in individuo:
+        for ind in populacao:
             avaliacoes.append(ind.avaliacao)
 
         val, idx = min((val, idx) for (idx, val) in enumerate(avaliacoes))
 
-        return individuo[idx]
+        return populacao[idx]
 
     def crossover(self, pai, mae):
 
         pai_bin = pai.x1 + pai.x2
         mae_bin = mae.x1 + mae.x2
 
-        corte = randint(0,12)
+        corte = randint(0, ag.nbits)
 
         filhos = []
 
@@ -93,42 +89,42 @@ class AG():
 
         return filhos
 
-    def mutacao(self):
+    def mutacao(self, populacao):
 
-        taxa_mutacao = 10
-        for ind in individuo:
-            if randint(0,100) <= taxa_mutacao:
+        for ind in populacao:
+            if randint(0,100) <= ag.taxa_mutacao:
                 ind_bin = ind.x1 + ind.x2
-                print("Antes ", ind_bin)
-                bit = randint(0, self.nbits)
+                # print("Antes  ", ind_bin)
+                bit = randint(0, self.nbits-1)
+                # print(f"vou mudar o bit {bit}")
+                # print(f"transformar {ind_bin[bit]} em {abs(int(ind_bin[bit]) - 1)}")
                 ind_bin[bit] = abs(int(ind_bin[bit]) - 1)
-                if bit > 6:
-                    ind.x1 = ind_int[:6]
-                else:
-                    ind.x2 = ind_int[6:]
-                print("depois ", ind.x1 + ind.x2)
+                ind.x1 = ind_bin[:6]
+                ind.x2 = ind_bin[6:]
+                # print("depois ", ind.x1 + ind.x2)
 
-ag  = AG(50, 100, 12)
+ag  = AG(50, 100, 10, 12)
 ag.geraPopulacao()
-individuo = ag.defineIndividuos()
-print(individuo[0].x1 + individuo[0].x2)
+nova_populacao = ag.defineIndividuos()
+# print(nova_populacao[0].x1 + nova_populacao[0].x2)
 
 
 
 for geracao in range(ag.num_geracoes):
-    print("Passei")
-    ag.avaliaPopulacao();
-    melhorIndividuo = ag.elitismo()
-    # print(f"Melhor individuo da geração {geracao} -> avaliação: {melhorIndividuo.avaliacao}")
+    # print("Passei")
+    ag.avaliaPopulacao(nova_populacao);
+    melhorIndividuo = ag.elitismo(nova_populacao)
+    print(f"Melhor individuo da geração {geracao} -> avaliação: {melhorIndividuo.avaliacao}")
 
+    antiga_populacao = nova_populacao
     nova_populacao = []
     while len(nova_populacao) < ag.tam_populacao:
-        pai = ag.torneio()
-        mae = ag.torneio()
+        pai = ag.torneio(antiga_populacao)
+        mae = ag.torneio(antiga_populacao)
         filhos = ag.crossover(pai, mae)
         nova_populacao.extend(filhos)
-        print("POP ", len(nova_populacao))
 
-    nova_populacao.pop(randint(0, 50))
+    # print("POP ", len(nova_populacao))
+    nova_populacao.pop(randint(0, len(nova_populacao)-1))
     nova_populacao.append(melhorIndividuo)
-    ag.mutacao()
+    ag.mutacao(nova_populacao)
